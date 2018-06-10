ac7b206acc054fd08c104a811e5a07beb5ba1da8
Amy Hart <xamyhart@gmail.com>
2018-06-10 15:36:18 +0100

    Objects file contained partner's answers, reverting to a clean slate to attempt the challenges from scratch

--- src/objects.js
+++ src/objects.js
@@ -1,62 +1,44 @@
 const createPerson = (name, age) => {
-    const user = {
-        name: (name),
-        age: (age)
-    }
-    return user
-};
-
-const getName = (object) => {
-    return object.name
-        // your code here
-};
-
-const getProperty = (property, object) => {
-    return object[property]
-        // your code here
-};
-
-const hasProperty = (property, object) => {
-    if (property in object) {
-        return true
-    } else {
-        return false
-    }
-};
-
-const isOver65 = (person) => {
-    if (person.age > 65) {
-        return true
-    } else {
-        return false
-    }
-};
-
-const getAges = (people) => {
-    return people.map(obj => obj.age)
-        // your code here
-};
-
-const findByName = (name, people) => {
-    return people.splice(obj => obj.name = name).filter(obj => obj.name === name).reduce((obj, item) => {
-            obj[people.name] = name
-        })
-        // your code here
-};
-
-const findHondas = (cars) => {
-    return cars
-};
-
-const averageAge = (people) => {
-    return people
-};
-
-const createTalkingPerson = (name, age) => {
-
-};
-
-module.exports = {
+    // your code here
+  };
+  
+  const getName = (object) => {
+    // your code here
+  };
+  
+  const getProperty = (property, object) => {
+    // your code here
+  };
+  
+  const hasProperty = (property, object) => {
+    // your code here
+  };
+  
+  const isOver65 = (person) => {
+    // your code here
+  };
+  
+  const getAges = (people) => {
+    // your code here
+  };
+  
+  const findByName = (name, people) => {
+    // your code here
+  };
+  
+  const findHondas = (cars) => {
+    // your code here
+  };
+  
+  const averageAge = (people) => {
+    // your code here
+  };
+  
+  const createTalkingPerson = (name, age) => {
+    // your code here
+  };
+  
+  module.exports = {
     createPerson,
     getName,
     getProperty,
@@ -67,4 +49,4 @@ module.exports = {
     findHondas,
     averageAge,
     createTalkingPerson
-}
+  }
